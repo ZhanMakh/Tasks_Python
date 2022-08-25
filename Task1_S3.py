a3cccab02b0a5979cd992bda3d072c1d2f17e3f5
Zhanetta Makhambetova <j_machambet@mail.ru>
2022-08-25 20:03:40 +0600

    Добавлено решение задачи № 1 семинара 3

---
+++ Task1_S3.py
@@ -0,0 +1,22 @@
+# Задача 1. Создайте файл. 
+# Запишите в него N первых элементов последовательности Фибоначчи.
+
+def myFibonacci(x):
+    if x in [1,2]:
+        return 1
+    else:
+        return myFibonacci(x-1)+myFibonacci(x-2)
+
+fib=[]
+for y in range(1,6):
+    fib.append(myFibonacci(y))
+    print(fib)
+
+fib = [1,1,2,3,5]
+with open("fibon.txt", "w") as data:
+    data.write("1\n")
+    data.write("1\n")
+    data.write("2\n")
+    data.write("3\n")
+    data.write("5\n")
+
